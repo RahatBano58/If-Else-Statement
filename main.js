1bf09504e5b367d5aed964f819aa49fac76fd7cc
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-19 21:26:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+"use strict";
+// If and Else Statement//
+let age = 18;
+if (age > 18) {
+    console.log("you are eligible for National ID Card");
+}
+else {
+    console.log("you are not eligible!");
+}
+; // you are not eligible
+let age1 = 20;
+if (age1 >= 18) {
+    console.log("you are eligible for National ID Card");
+}
+else {
+    console.log("you are not eligible");
+}
+; // you are eligible for National ID Card
+let mark = 70;
+if (mark <= 80) {
+    console.log("mark in science");
+}
+else {
+    console.log("mark in mathematics");
+}
+; // Mark in science
+let isRaining = "True";
+if (isRaining) {
+    console.log("Wear a rain coat");
+}
+else {
+    console.log("Wear a sunglasses");
+}
+; // Wear a rain coat
+let marksEnglish = 80;
+if (marksEnglish == 90) {
+    console.log("you got A grade");
+}
+else {
+    console.log("you are not get A1 grade ");
+}
+; // you are not get A1 grade 
+let email = "rahatbano142@gmail.com";
+let pass = "123456";
+if (email == "rahatbano142@gmail.com" && pass == "123456") {
+    console.log("Successfully Logged In");
+}
+else {
+    console.log("Invalid Creds");
+}
---
+++ main.ts
@@ -0,0 +1,51 @@
+// If and Else Statement//
+
+let age = 18;
+
+if(age > 18){
+    console.log("you are eligible for National ID Card")
+}else {
+    console.log("you are not eligible!")
+};                                       // you are not eligible
+ 
+let age1:number = 20;
+
+if(age1 >= 18){
+    console.log("you are eligible for National ID Card")
+}else{
+    console.log("you are not eligible")
+};                                         // you are eligible for National ID Card
+
+
+let mark = 70
+
+if(mark <= 80){
+    console.log("mark in science")
+}else{
+    console.log("mark in mathematics")       
+};                                      // Mark in science
+
+
+let isRaining = "True"
+if(isRaining){
+    console.log("Wear a rain coat")
+}else{
+    console.log("Wear a sunglasses") 
+};                                    // Wear a rain coat
+
+let marksEnglish:number =80
+if(marksEnglish == 90){
+    console.log("you got A grade")
+}else{
+    console.log("you are not get A1 grade ")
+};                                     // you are not get A1 grade 
+
+let email:string = "rahat2@gmail.com";
+let pass: string = "123456"
+
+if (email == "rahat2@gmail.com" && pass == "123456" ){
+    console.log("Successfully Logged In");
+}
+else {
+    console.log("Invalid Creds");
+}
